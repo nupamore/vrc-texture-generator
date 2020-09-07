6501b2574be68863daa6b474aa45df0efbac5fc6
Sangbaek Lee <nupamore@gmail.com>
2020-09-07 14:13:13 +0900

    639f497

---
+++ _nuxt/pages/index.6c795d9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{314:function(t,n,e){"use strict";e.r(n);var r={},l=e(43),component=Object(l.a)(r,(function(){var t=this.$createElement,n=this._self._c||t;return n("div",{staticClass:"container"},[n("div",[n("nuxt-link",{attrs:{to:"/editor"}},[this._v("editor")])],1)])}),[],!1,null,null,null);n.default=component.exports}}]);
